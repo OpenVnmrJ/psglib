e7277b5ca65d02590a6c92efe03c52a52f0e6d2d
Tim Burrow <timburrow@me.com>
2016-01-11 23:09:39 -0500

    goesytr v1.0
    Double pulsed field gradient spinecho 1D ROE does transient 1D ROE
    experiment where very weak ROE's are visible because gradients dephase
    unwanted signals.  Selective pulses on desired multiplet can be easily
    created using setshape macro. Parameters are automatically determined
    and installed. The setshape macro can be run from ds_1 menu. No WFG
    is required when using VNMR 5.3 or later

---
+++ psglib/goesytr.c
@@ -0,0 +1,203 @@
+/* goesytr    - selective transverse cross-relaxation experiment
+               in rotating frame 
+		
+
+   ref:  Shaka, et. al., JACS, 114, 3157 (1992).
+
+
+Krish Krishnamurthy     18 Nov. 1994
+    (selective, from Krishnamurthy selexcit.c gg 2/96    */
+
+
+#include <standard.h>
+
+
+static int ph1[4] = {1,3,2,0},
+           ph2[8] = {1,1,2,2,3,3,0,0},
+           ph3[4] = {1,3,2,0};
+
+pulsesequence()
+{
+   double          arraydim,
+                   ss,
+                   slpwr,
+                   slpw,
+                   selpw,selpwr,gzlvl1,gtime1,gzlvl2,gtime2,gzlvl3,gtime3,gstab1,
+                   mix,
+                   phase,
+                   cycles;
+   int             iphase;
+   char            sspul[MAXSTR],
+                selshape[MAXSTR],selflg[MAXSTR],homodec[MAXSTR],
+                   satmode[MAXSTR];
+
+
+/* LOAD AND INITIALIZE PARAMETERS */
+   arraydim = getval("arraydim");
+   mix = getval("mix");
+   selpw = getval("selpw");
+   selpwr = getval("selpwr");
+   gzlvl1 = getval("gzlvl1");
+   gzlvl2 = getval("gzlvl2");
+   gzlvl3 = getval("gzlvl3");
+      gtime1 = getval("gtime1");
+      gtime2 = getval("gtime2");
+      gtime3 = getval("gtime3");
+    gstab1 = getval("gstab1");
+   phase = getval("phase");
+   iphase = (int) (phase + 0.5);
+   slpwr = getval("slpwr");
+   slpw = getval("slpw");
+   ss = getval("ss");
+   getstr("selshape",selshape);
+   getstr("selflg",selflg);
+   getstr("homodec",homodec);
+
+   getstr("sspul", sspul);
+   getstr("satmode",satmode);
+  
+
+/* DETERMINE STEADY-STATE MODE */
+   if (ss < 0)
+      ss = (-1) * ss;
+   else
+    {
+      if ((ss > 0) && (ix == 1))
+	 ss = ss;
+      else
+	 ss = 0;
+    }
+   initval(ss, ssctr);
+   initval(ss, ssval);
+
+
+/* STEADY-STATE PHASECYCLING
+/* This section determines if the phase calculations trigger off of (SS - SSCTR)
+   or off of CT */
+
+   ifzero(ssctr);
+      assign(ct,v7);
+   elsenz(ssctr);
+      sub(ssval, ssctr, v7);	/* v7 = 0,...,ss-1 */
+   endif(ssctr);
+
+   settable(t1,4,ph1);
+   settable(t2,8,ph2);
+   settable(t3,4,ph3);
+
+   getelem(t1,v7,v1);
+   getelem(t2,v7,v2);
+   add(v2,two,v3);
+   getelem(t3,v7,oph);
+   add(v1,zero,v11); add(v11,two,v12);
+   if (iphase == 2)
+      incr(v1);			/* hypercomplex method */
+   if (iphase == 3)
+      initval((double) (ix-1)/(arraydim/ni), v14);	/* TPPI method */
+      add(v1, v14, v1);
+
+     initval(2.0*(double)(((int)(d2*getval("sw1")+0.5)%2)),v13);
+     if ((iphase == 1) || (iphase == 2))
+      {
+       add(v1,v13,v1);
+       add(oph,v13,oph);
+      }
+
+   cycles = mix / ((4.0 * slpw) + 4.0e-6);
+   initval(cycles, v10);	/* mixing time cycles */
+
+
+/* BEGIN ACTUAL PULSE SEQUENCE */
+   status(A);
+     obspower(tpwr);
+     delay(50.0e-6);
+
+     if (sspul[0] == 'y')
+      { 
+	zgradpulse(gzlvl3,2.0e-3);
+	rgpulse(pw,zero,rof1,rof1);
+	zgradpulse(gzlvl3,2.0e-3);
+      }
+     delay(d1);
+
+     if (satmode[0] == 'y')
+      {
+	obspower(satpwr);
+	if (satfrq != tof)
+	 obsoffset(satfrq);
+	rgpulse(satdly,zero,10.0e-6,10.0e-6);
+	if (satfrq != tof)
+	 obsoffset(tof);
+	obspower(tpwr);
+       }
+   status(B);
+     rgpulse(pw, v1, rof1, 1.0e-6);
+     if (selflg[0] == 'y')
+      {
+       if (homodec[0] == 'y')
+       {
+	if (d2/2 > 0.0)
+		delay(d2/2 - (2*pw/PI) - pw - 2.0e-6);
+	else
+		delay(d2/2);
+        rgpulse(2*pw,v11,1.0e-6,1.0e-6);
+       }
+       else
+       {
+	if (d2 > 0.0)
+		delay(d2 - (2*pw/PI) - 1.0e-6);
+	else
+		delay(d2);
+       }
+        delay(gstab1);
+        zgradpulse(gzlvl1,gtime1);
+        delay(gstab1);
+        obspower(selpwr);
+        shaped_pulse(selshape,selpw,v11,rof1,rof1); 
+        obspower(tpwr);
+        delay(gstab1); 
+        zgradpulse(gzlvl1,gtime1);
+        delay(gstab1);
+       if (homodec[0] == 'y')
+       {
+	if (d2/2 > 0.0)
+		delay(d2/2 - pw - 1.0e-6);
+	else
+		delay(d2/2);
+       }
+        delay(gstab1); 
+        zgradpulse(gzlvl2,gtime2);
+        delay(gstab1); 
+        obspower(selpwr); 
+        shaped_pulse(selshape,selpw,v12,rof1,rof1);  
+        delay(gstab1); 
+        zgradpulse(gzlvl2,gtime2);
+        delay(gstab1);
+      }
+     else
+      {
+	if (d2 > 0.0) 
+                delay(d2 - (2*pw/PI) - 1.0e-6 - POWER_DELAY); 
+        else 
+                delay(d2); 
+       }
+      rcvroff();
+      rlpower(slpwr,TODEV);
+      if (cycles > 1.5000)
+       {
+		xmtron();
+         starthardloop(v10);
+		txphase(v2);
+		delay(2*slpw);
+		txphase(v3);
+		delay(2*slpw);
+         endhardloop();
+		xmtroff();
+       }
+
+      rlpower(tpwr, TODEV);
+      delay(rof2);
+      rcvron();
+
+   status(C);
+}
